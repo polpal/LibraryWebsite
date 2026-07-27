dd1f1c15c85c3811ee0847894bc40427d1c2c365
polpal <partho.sarkar.cosh@gmail.com>
2026-07-27 16:18:24 +0530

    lesson 2: Create first Flask application

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route("/")
+@app.route("/")
+def home():
+    return "Welcome to the Library Management System!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
