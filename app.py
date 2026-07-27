c785274a9b8fbf4af86d9ac12d6c05ce1bfd67d2
polpal <partho.sarkar.cosh@gmail.com>
2026-07-27 17:15:49 +0530

    Lession 3: Add HTML templates and render first webpage

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 @app.route("/")
 @app.route("/")
 def home():
-    return "Welcome to the Library Management System!"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
