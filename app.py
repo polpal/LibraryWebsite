d9347c90fd01886dac81978716fa16c6b69d6a64
polpal <partho.sarkar.cosh@gmail.com>
2026-07-27 17:56:11 +0530

    Lesson 4: Add template inheritance and multiple pages

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+# Disable template caching during development
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
 def home():
