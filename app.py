fda26d334773304e39b7e97b97547305a0b04a03
polpal <partho.sarkar.cosh@gmail.com>
2026-07-27 17:54:59 +0530

    Lesson 4: Add template inheritance and multiple pages

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+
 app = Flask(__name__)
-@app.route("/")
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
+@app.route("/books")
+def books():
+    return render_template("books.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
