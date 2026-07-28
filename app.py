e0c56ee14e3ca177dc83f7d027113c1feead6642
polpal <partho.sarkar.cosh@gmail.com>
2026-07-28 18:31:12 +0530

    Lesson 6 Part 2: Read library books from Excel using Pandas

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
 from flask import Flask, render_template
-
+import pandas as pd
 app = Flask(__name__)
-# Disable template caching during development
-app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
 def home():
-    return render_template("index.html")
 
-@app.route("/books")
-def books():
-    return render_template("books.html")
+    df = pd.read_excel("books.xlsx")
+    books = df.to_dict(orient="records")
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+    return render_template("index.html", books=books)
 
 if __name__ == "__main__":
     app.run(debug=True)
