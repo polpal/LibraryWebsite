cc5c48245801fc1ab47984558c4624f6a1883fea
polpal <partho.sarkar.cosh@gmail.com>
2026-07-31 14:00:54 +0530

    Lesson 11 - Database design completed

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pandas as pd
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
 
+    search = request.args.get("search", "").strip()
+
     df = pd.read_excel("books.xlsx")
+    print(df.columns)
+
+    if search:
+
+        mask = (
+            df["Book"].astype(str).str.contains(search, case=False, na=False)
+            |
+            df["Author"].astype(str).str.contains(search, case=False, na=False)
+        )
+
+        df = df[mask]
+
     books = df.to_dict(orient="records")
 
-    return render_template("index.html", books=books)
+    return render_template(
+        "index.html",
+        books=books,
+        search=search
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
